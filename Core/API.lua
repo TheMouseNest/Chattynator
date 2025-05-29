55fe4f446bfcebbc29595fc6ed1316ee64a1dd2a
plusmouse <plusmouse@protonmail.com>
2025-05-29 16:52:12 +0100

    Add dynamic filter API

--- Core/API.lua
+++ Core/API.lua
@@ -8,3 +8,25 @@ Chattynator = {
 function Chattynator.API.GetHyperlinkHandler()
   return ChattynatorHyperlinkHandler
 end
+
+local funcToWrapper = {
+}
+-- TODO: Test this
+function Chattynator.API.AddDynamicFilter(func)
+  local wrapper
+  wrapper = function(data)
+    local state = xpcall(function() func(data) end, CallErrorHandler)
+    if not state then
+      Chattynator.API.RemoveDynamicFilter(wrapper)
+    end
+  end
+  funcToWrapper[func] = wrapper
+  addonTable.Messages.AddLiveModifier(wrapper)
+end
+
+function Chattynator.API.RemoveDynamicFilter(func)
+  if funcToWrapper[func] then
+    addonTable.Messages.RemoveLiveModifier(funcToWrapper[func])
+    funcToWrapper[func] = nil
+  end
+end

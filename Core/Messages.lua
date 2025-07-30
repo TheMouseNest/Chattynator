c44c0d0fb1ec9dafc508a1bce732d212d9d6767f
plusmouse <plusmouse@protonmail.com>
2025-07-30 12:36:49 +0100

    Fallback for when an addon attempts to insert a nil message

--- Core/Messages.lua
+++ Core/Messages.lua
@@ -723,7 +723,7 @@ function addonTable.MessagesMonitorMixin:GetFont() -- Compatibility with any emo
 end
 
 function addonTable.MessagesMonitorMixin:AddMessage(text, r, g, b, _, _, _, _, _, Formatter)
-  if text == "" then
+  if text == "" or type(text) ~= "string" then
     if not self.lockType then
       self.incomingType = nil
     end

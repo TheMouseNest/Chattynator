8fe693fa94bbc93457ba6f33ffac1c62e3bff28a
plusmouse <plusmouse@protonmail.com>
2025-09-28 22:34:58 +0100

    [Fixes #91] Add "/run or WAs" option to per-tab addon output settings

--- CustomiseDialog/Filtering.lua
+++ CustomiseDialog/Filtering.lua
@@ -253,6 +253,7 @@ function addonTable.CustomiseDialog.SetupTabFilters(parent)
             table.insert(fields, {name, StripHyperlinks(title)})
           end
         end
+        table.insert(fields, {"/loadstring", addonTable.Locales.RUN_OR_WA})
         table.sort(fields, function(a, b) return a[2]:lower() < b[2]:lower() end)
         if tab.invert then
           for _, f in ipairs(fields) do

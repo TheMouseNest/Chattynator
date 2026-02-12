c6ff730a85936bc3723664856ba3616842edc725
plusmouse <plusmouse@protonmail.com>
2026-02-12 20:00:27 +0000

    Fix issecretvalue call on classic

--- Core/Messages.lua
+++ Core/Messages.lua
@@ -1305,7 +1305,7 @@ function addonTable.MessagesMonitorMixin:MessageEventHandler(event, ...)
         playerLinkDisplayText = playerWrapper:format(coloredName);
       end
 
-      local isCommunityType = (not issecretvalue or not issecretvalue(type)) and type == "COMMUNITIES_CHANNEL" or issecretvalue(type);
+      local isCommunityType = (not issecretvalue or not issecretvalue(type)) and type == "COMMUNITIES_CHANNEL" or (issecretvalue and issecretvalue(type))
       if ( isCommunityType ) then
         local isBattleNetCommunity = bnetIDAccount ~= nil and bnetIDAccount ~= 0;
         local messageInfo, clubId, streamId, clubType = C_Club.GetInfoFromLastCommunityChatLine();
